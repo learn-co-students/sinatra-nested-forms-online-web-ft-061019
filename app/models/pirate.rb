90332861d39d67bb361e68cd929508d5273841c9
Jeremiah Rodden <jrodden1@gmail.com>
2019-07-30 16:20:16 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+   attr_accessor :name, :weight, :height
+
+   def initialize(params_hash)
+      self.name = params_hash[:name]
+      self.weight = params_hash[:weight]
+      self.height = params_hash[:height]
+      @@all << self
+   end
+   
+   @@all = []
+
+   def self.all
+      @@all
+   end
+
 end
