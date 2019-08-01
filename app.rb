1bbbaf3fb141c08f05f7c1fb16eca4e89039b2cf
Mic Fox <michael.c.fox.ii@gmail.com>
2019-08-01 10:16:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = params['pirate']['name']
+      @pirate_weight = params['pirate']['weight']
+      @pirate_height = params['pirate']['height']
+      @first_ship = params['pirate']['ships'][0]['name']
+      @first_type = params['pirate']['ships'][0]['type']
+      @first_booty = params['pirate']['ships'][0]['booty']
+      @second_ship = params['pirate']['ships'][1]['name']
+      @second_type = params['pirate']['ships'][1]['type']
+      @second_booty = params['pirate']['ships'][1]['booty']
+      erb :'/pirates/show'
+    end
   end
 end
