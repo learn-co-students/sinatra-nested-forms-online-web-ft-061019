2e1dbfe40229c9ffabdf1aec0a83a825c0b47d7c
github username <github email address>
2019-08-05 17:54:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
       
-      params[:pirate][:ships].each do |ship_details|
-        Ship.new(ship_details)
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end 
 
       @ships = Ship.all
